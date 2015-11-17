9d2a429168e1e26831697ecb3629a26a22e267ec
Marco Musella <mar.musella@gmail.com>
2015-11-17 21:03:03 +0100

    implementatation of jsonpatchdocument

--- Classes/JSONPatchDocument.h
+++ Classes/JSONPatchDocument.h
@@ -8,6 +8,24 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum JSONPatchOperation{
+    
+    JSONPatchOperationAdd = 0,
+    JSONPatchOperationRemove,
+    JSONPatchOperationReplace,
+    JSONPatchOperationMove,
+    JSONPatchOperationCopy,
+    JSONPatchOperationTest
+
+}JSONPatchOperation;
+
 @interface JSONPatchDocument : NSObject
+
+@property (nonatomic, assign) JSONPatchOperation op;
+@property (nonatomic, strong) NSString *path;
+@property (nonatomic, strong) id value;
+
+- (NSString *) convertToJsonStringWithError: (NSError **) error;
+- (NSData *) convertToJsonDataWithError: (NSError **) error;
 
 @end
