47ae67d540d63f69d5fc0ef64d09c5687096e3da
Marco Musella <mar.musella@gmail.com>
2015-11-19 15:25:32 +0100

    remove unused samples

--- Project/JSONAPI/ViewController.m
+++ Project/JSONAPI/ViewController.m
@@ -10,10 +10,6 @@
 
 #import "JSONAPI.h"
 
-#import "CommentResource.h"
-#import "PeopleResource.h"
-#import "PostResource.h"
-
 @interface ViewController (){
 
 }
@@ -24,39 +20,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    JSONAPIClient *client = [JSONAPIClient new];
-    client.endpoint = @"http://beta.organizations.bdms.qurami.net";
-    
-    NSArray *resourcesToInclude = @[@"offices"];
-    
-    [client getJSONAPIDocumentWithPath:@"/app/organizations" includedResourceTypes: resourcesToInclude completionHandler:^(JSONAPIDocument *jsonApi, NSInteger statusCode, NSError *error) {
-        
-        
-        if(!error){
-            
-            JSONAPIResource *quramiResource;
-            if(jsonApi.data){
-                NSArray *allResources = (NSArray *) jsonApi.data;
-                for (JSONAPIResource *thisResource in allResources) {
-                    if([thisResource.ID isEqualToString:@"1"]){
-                        quramiResource = thisResource;
-                        break;
-                    }
-                }
-                
-                
-                NSArray *quramiOffices = [quramiResource getRelatedResourcesFromJSONAPIResourcesArray: jsonApi.included];
-                NSLog(@"resource %@ has %ld included resources", quramiResource.attributes[@"name"], [quramiOffices count]);
-            }
-            
-        }
-        else{
-            
-            NSLog(@"error returned: %@", error.localizedDescription);
-        
-        }
-    }];
     
 }
 
